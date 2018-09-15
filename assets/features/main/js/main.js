20e4ac5f4962ddbfeec9cf246fab05f855116d90
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2018-09-15 23:31:04 +0430

    Landing is ready !

--- assets/features/main/js/main.js
+++ assets/features/main/js/main.js
@@ -1,3 +1,48 @@
+$('#owl-one').owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 10000,
+  smartSpeed: 900,
+  navText: ["<i class='fa fa-chevron-right'></i>", "<i class='fa fa-chevron-left'></i>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+
+$('#owl-two').owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: false,
+  autoplayTimeout: 10000,
+  smartSpeed: 900,
+  navText: ["<i class='fa fa-chevron-right'></i>", "<i class='fa fa-chevron-left'></i>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+
+
 $(document).ready(function () {
   $('a.traveller').on('click', function (e) {
     // ambil isi href
