7c96e0ea6bfac52241a4bde1fd22112d6ae58932
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2018-09-18 15:00:53 +0430

    Forms are ready to use ! responsive

--- assets/features/main/js/main.js
+++ assets/features/main/js/main.js
@@ -56,3 +56,18 @@ $(document).ready(function () {
     e.preventDefault();
   });
 });
+
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+    $('#return-to-top').fadeIn(200); // Fade in the arrow
+  } else {
+    $('#return-to-top').fadeOut(200); // Else fade out the arrow
+  }
+});
+$('#return-to-top').click(function () { // When arrow is clicked
+  $('body,html').animate({
+    scrollTop: 0 // Scroll to top of body
+  }, 500);
+});
